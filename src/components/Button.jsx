d28f457b116cf2939f0866abd2c5334a6be56de7
BSCE20004_Farhan <73163937+farhan26azam@users.noreply.github.com>
2023-06-25 14:37:40 +0500

    Code and Assets

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+
+const Button = ({styles}) => {
+  return (
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+      Get Started
+    </button>
+  )
+}
+
+export default Button
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+    Navbar,
+    Business,
+    Billing,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Stats,
+    Testimonials,
+    Button
+};
